3b816613a981ec917ba5855a7233d60381f64f84
Bukola Olafenwa <bukolaolafenwa@gmail.com>
2026-08-12 23:18:32 +0100

    feat: add organizer attendee list

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ import paymentRoutes from './routes/payment.routes.js'
 import { startOrderExpiryJob } from "./jobs/orderExpiry.js";
 import reservationRoutes from './routes/reservation.routes.js'
 import checkInRoutes from './routes/checkin.routes.js'
-
+import attendeeRoutes from './routes/attendee.routes.js'
 
 import {
   appErrorHandler,
@@ -182,6 +182,7 @@ app.use('/api/v1/events', checkoutRoutes)
 app.use('/api/v1/events', tickettypeRoutes)
 app.use('/api/v1/events', reservationRoutes)
 app.use('/api/v1/events', checkInRoutes)
+app.use('/api/v1/events', attendeeRoutes)
 app.use('/api/v1/events', eventRoutes)
 
 
